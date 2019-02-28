0bdbb3ef6ea5094192bfb41fcea1ed32383fc654
zhou <prince5460@126.com>
2019-02-28 10:24:07 +0800

    register

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -6,7 +6,9 @@
 '''
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
+from wtforms import ValidationError
+from ..models import User
 
 
 class LoginForm(FlaskForm):
@@ -14,3 +16,24 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log In')
+
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
+    # WTForms 提供的 Regexp 验证函数，确保 username 字段的值以字母数字开头，而且只包含字母、数字、下划线和点号。
+    # 这个验证函数中正则表达式后面的两个参数分别是正则表达式的标志和验证失败时显示的错误消息。
+    username = StringField('Username', validators=[DataRequired(), Length(1, 64),
+                                                   Regexp('^[A-Za-z0-9][A-Za-z0-9_.]*$', 0,
+                                                          'Usernames must have only letters,numbers,dots or underscores')])
+    password = PasswordField('Password', validators=[DataRequired(), Length(6, 64),
+                                                     EqualTo('password2', message='Passwords must match.')])
+    password2 = PasswordField('Confirm password', validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered.')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username already in use.')
